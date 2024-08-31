8e0ed0f4de3a5baa63ad72f8aaa2a7732f8bfe3b
AnmolPdy <anmolpdy04@gmail.com>
2024-08-31 06:02:01 +0530

    updated main component

--- Angular_task/dynamic-table-app/src/app/app.component.ts
+++ Angular_task/dynamic-table-app/src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { filter } from 'rxjs';
+import { ProductComponent } from './product/product.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,UsersComponent,ProductComponent,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'dynamic-table-app';
+
+  isHomePage: boolean = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      this.isHomePage = this.router.url === '/';
+    });
+  }
+
+  navigateTo(route: string): void {
+    this.router.navigate([route]);
+  }
 }
--- Angular_task/dynamic-table-app/src/app/app.routes.ts
+++ Angular_task/dynamic-table-app/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { ProductComponent } from './product/product.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'users', component: UsersComponent}, 
+    { path: 'products', component: ProductComponent},
+];
